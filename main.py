0067d763f5f705c9302243d65d63727c12fd5a4f
Kenny Gilvary <kennygilvary82@gmail.com>
2025-01-09 13:58:00 -0500

    basic first question

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import random
+
+# List of categories and answers
+QUESTIONS = [
+    {"category": "Around the House", "answers": [
+        "A Glass Of Bourbon"
+    ]}
+]
+
+# main function
 def main():
     play_game()
 
+# game function
 def play_game():
-    print("game started")
+    # prompt user to play game and amt of players
+    print("Welcome to Wheel Of Fortune!")
+    num_players = input("How many players? (Type 1, 2, or 3): ")
+
+    # display question, player, money, and options (spin, solve, buy vowel)
+    print("\n_  _ _ _ _ _  _ _  _ _ _ _ _ _ _\n\n")
+    print(f"The category is: {QUESTIONS[0]["category"]}\n\n")
+    print("How much you have this round: $4000")
+    print("Current winnings: $1234")
+    player_choice = input("What would you like to do? (1: Spin, 2: Solve, 3: Buy Vowel): ")
 
 main()
